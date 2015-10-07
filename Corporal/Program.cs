653b1c578813f336c781d7491d69aa7b207da3c7
Sven Rütz <ruetz@itisim.com>
2015-10-07 17:28:29 +0200

    initial commit

---
+++ Corporal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporal
+{
+    class Program
+    {
+        static Queue<Task> queue = new Queue<Task>();
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
